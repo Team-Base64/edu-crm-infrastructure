9a9d7689c18926b028b9f171111fffbed0fc2fed
Leonard Pak <leopak2000@gmail.com>
2023-10-26 21:42:52 +0300

    fix sql [DEPLOY]

--- sql/init.sql
+++ sql/init.sql
@@ -51,4 +51,4 @@ INSERT INTO students (inviteHash, name) VALUES
 ('d0e8d5bc-6de7-11ee-b962-0242ac120002', 'bbb');
 
 INSERT INTO chats (id, teacherID, studentHash) VALUES
-(1, 2, 'd0e8d5bc-6de7-11ee-b962-0242ac120002');
+(1, 1, 'd0e8d5bc-6de7-11ee-b962-0242ac120002');
