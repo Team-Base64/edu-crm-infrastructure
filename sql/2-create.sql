792ece0f8d2779041fcc4d50a85205ca3d9bf616
Leonard Pak <leopak2000@gmail.com>
2023-11-05 20:45:02 +0300

    students classes

--- sql/2-create.sql
+++ sql/2-create.sql
@@ -1,38 +1,52 @@
-CREATE TABLE teachers (
-    id  SERIAL PRIMARY KEY,
-    login VARCHAR (50) UNIQUE NOT NULL,
-    password VARCHAR (30),
-	name VARCHAR (100) NOT NULL
-);
+CREATE TYPE SOCIAL AS ENUM('tg', 'vk');
 
-CREATE TABLE students (
-    id  SERIAL PRIMARY KEY,
-	name VARCHAR (100) NOT NULL
-);
+CREATE TABLE
+    teachers (
+        id SERIAL PRIMARY KEY,
+        login VARCHAR(50) UNIQUE NOT NULL,
+        password VARCHAR(30),
+        name VARCHAR(100) NOT NULL
+    );
 
-CREATE TABLE chats
-(
-    id       SERIAL PRIMARY KEY,
-    teacherID   INT REFERENCES teachers (id) ON DELETE CASCADE,
-    studentID   INT REFERENCES students (id) ON DELETE CASCADE
-);
+CREATE TABLE
+    students (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(100) NOT NULL,
+        socialType SOCIAL NOT NULL
+    );
 
-CREATE TABLE messages
-(
-    id          SERIAL PRIMARY KEY,
-    chatID      INT REFERENCES chats (id) ON DELETE CASCADE,
-    text        TEXT NOT NULL,
-    isAuthorTeacher    BOOLEAN     NOT NULL,
-    attaches    VARCHAR[],
-    time TIMESTAMP NOT NULL,
-    isRead BOOLEAN NOT NULL DEFAULT FALSE
-);
+CREATE TABLE
+    chats (
+        id SERIAL PRIMARY KEY,
+        teacherID INT REFERENCES teachers (id) ON DELETE CASCADE,
+        studentID INT REFERENCES students (id) ON DELETE CASCADE,
+        UNIQUE (teacherID, studentID)
+    );
 
-CREATE TABLE classes
-(
-    id SERIAL PRIMARY KEY,
-    teacherID INT REFERENCES teachers(id) ON DELETE CASCADE,
-    title VARCHAR (100) NOT NULL,
-    description TEXT,
-    inviteToken CHAR (10) NOT NULL
-);
+CREATE TABLE
+    messages (
+        id SERIAL PRIMARY KEY,
+        chatID INT REFERENCES chats (id) ON DELETE CASCADE,
+        text TEXT NOT NULL,
+        isAuthorTeacher BOOLEAN NOT NULL,
+        attaches VARCHAR[],
+        time TIMESTAMP NOT NULL,
+        isRead BOOLEAN NOT NULL DEFAULT FALSE
+    );
+
+CREATE TABLE
+    classes (
+        id SERIAL PRIMARY KEY,
+        teacherID INT REFERENCES teachers (id) ON DELETE CASCADE,
+        title VARCHAR(100) NOT NULL,
+        description TEXT,
+        inviteToken CHAR(10) UNIQUE NOT NULL
+    );
+
+CREATE TABLE
+    classes_students (
+        id SERIAL PRIMARY KEY,
+        classID INT REFERENCES classes (id) ON DELETE CASCADE,
+        studentID INT REFERENCES students (id) ON DELETE CASCADE,
+        UNIQUE (classID, studentID)
+    );
--- sql/3-insert.sql
+++ sql/3-insert.sql
@@ -1,20 +1,61 @@
-INSERT INTO teachers (login, name, password) VALUES
-('art@art',	'aaa', '123');
+INSERT INTO
+  teachers (login, name, password)
+VALUES
+  ('art@art', 'mockTeacher', '123');
 
-INSERT INTO students (name) VALUES
-('bbb'),
-('bbb2');
+INSERT INTO
+  students (name, socialType)
+VALUES
+  ('mockStudent1', 'tg'),
+  ('mockStudent2', 'vk');
 
-INSERT INTO chats (teacherID, studentID) VALUES
-(1, 1),
-(1, 2);
+INSERT INTO
+  chats (teacherID, studentID)
+VALUES
+  (1, 1),
+  (1, 2);
 
-INSERT INTO messages (chatID, text, isAuthorTeacher, time, isRead) VALUES
-(1, 'mes1', true, '2017-03-17 19:43:35.178882-07', true),
-(1, 'mes2', false, '2020-03-17 19:22:01.178882-07', false),
-(2, 'mes21', true, '2017-03-17 19:43:35.178882-07', true),
-(2, 'mes22', false, '2020-03-17 19:22:01.178882-07', false);
+INSERT INTO
+  messages (chatID, text, isAuthorTeacher, time, isRead)
+VALUES
+  (
+    1,
+    'mes1',
+    true,
+    '2017-03-17 19:43:35.178882-07',
+    true
+  ),
+  (
+    1,
+    'mes2',
+    false,
+    '2020-03-17 19:22:01.178882-07',
+    false
+  ),
+  (
+    2,
+    'mes21',
+    true,
+    '2017-03-17 19:43:35.178882-07',
+    true
+  ),
+  (
+    2,
+    'mes22',
+    false,
+    '2020-03-17 19:22:01.178882-07',
+    false
+  );
 
-INSERT INTO classes (teacherID, title, description, inviteToken) VALUES
-(1, 'Class 1', 'My first class', '1234567890'),
-(1, 'Class 2', 'My second class', 'qwertyuiop');
+INSERT INTO
+  classes (teacherID, title, description, inviteToken)
+VALUES
+  (1, 'Class 1', 'My first class', '1234567890'),
+  (1, 'Class 2', 'My second class', 'qwertyuiop');
+
+INSERT INTO
+  classes_students (classID, studentID)
+VALUES
+  (1, 1),
+  (2, 1),
+  (2, 2);
