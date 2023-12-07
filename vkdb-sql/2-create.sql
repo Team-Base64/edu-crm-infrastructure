ab7176077997fd7349b82a93b4fae030d04e4ea9
Leonard Pak <leopak2000@gmail.com>
2023-12-07 13:40:20 +0300

    make internal_chat_id field nullable at vkdb

--- vkdb-sql/2-create.sql
+++ vkdb-sql/2-create.sql
@@ -1,24 +1,25 @@
-create type bot_type as enum ('M', 'S');
+create type bot_type as enum('M', 'S');
 
-create table vk_bots (
-    id serial primary key ,
-    token varchar not null ,
-    vk_group_id serial not null,
-    bot_type bot_type not null
-);
+create table
+    vk_bots (
+        id serial primary key,
+        token varchar not null,
+        vk_group_id int not null,
+        bot_type bot_type not null
+    );
 
-create table link_user_student
-(
-    vk_user_id serial not null ,
-    student_id serial not null ,
-        unique(vk_user_id, student_id)
-);
+create table
+    link_user_student (
+        vk_user_id int not null,
+        student_id int not null,
+        unique (vk_user_id, student_id)
+    );
 
-create table link_user_bot_chat (
-    vk_user_id serial not null ,
-    vk_group_id serial not null ,
-    internal_chat_id serial not null,
-    class_id serial not null,
-    unique (vk_group_id, vk_user_id, internal_chat_id)
-);
-
+create table
+    link_user_bot_chat (
+        vk_user_id int not null,
+        vk_group_id int not null,
+        internal_chat_id int null,
+        class_id int not null,
+        unique (vk_group_id, vk_user_id, internal_chat_id)
+    );
